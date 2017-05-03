19f1fc1aea5057083604c8a7b0c347d269dfc2fe
cucy <292016176@qq.com>
2017-05-03 11:13:19 +0800

    requests模拟知乎登录

--- ArticleSpider/ArticleSpider/utils/zhihu_login_requests.py
+++ ArticleSpider/ArticleSpider/utils/zhihu_login_requests.py
@@ -3,6 +3,7 @@
 # Time: 2017/5/2 18:15
 # Author: zhourudong
 import requests
+
 try:
     import cookielib
 except:
@@ -15,28 +16,30 @@ session.cookies = cookielib.LWPCookieJar(filename="cookies.txt")
 try:
     session.cookies.load(ignore_discard=True)
 except:
-    print ("cookie未能加载")
+    print("cookie未能加载")
 
 agent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0"
 header = {
-    "HOST":"www.zhihu.com",
+    "HOST": "www.zhihu.com",
     "Referer": "https://www.zhizhu.com",
     'User-Agent': agent
 }
 
+
 def is_login():
-    #通过个人中心页面返回状态码来判断是否为登录状态
+    # 通过个人中心页面返回状态码来判断是否为登录状态
     inbox_url = "https://www.zhihu.com/question/56250357/answer/148534773"
     response = session.get(inbox_url, headers=header, allow_redirects=False)
     a = response.content
     print(type(a))
-    if response.status_code  != 200:
+    if response.status_code != 200:
         return False
     else:
         return True
 
+
 def get_xsrf():
-    #获取xsrf code
+    # 获取xsrf code
     response = session.get("https://www.zhihu.com", headers=header)
     match_obj = re.match('.*name="_xsrf" value="(.*?)"', response.text)
     if match_obj:
@@ -46,24 +49,49 @@ def get_xsrf():
 
 
 def get_index():
-    response = session.get("https://www.zhihu.com", headers=header )
+    response = session.get("https://www.zhihu.com", headers=header)
     with open("index_page.html", "wb") as f:
         f.write(response.text.encode("utf-8"))
-    print ("ok")
+    print("ok")
+
+
+def get_captcha():
+    ''' 获取验证码'''
+    import time
+    t = str(int(time.time() * 1000))
+    captcha_url = "https://www.zhihu.com/captcha.gif?r={0}&type=login".format(t)
+    t = session.get(captcha_url, headers=header)
+    with open("captcha.jpg", 'wb') as f:
+        f.write(t.content)
+        f.close()
+
+    from PIL import Image
+    try:
+        im = Image.open('captcha.jpg')
+        im.show()
+        im.close()
+    except Exception as e:
+        print(e)
+        pass
+
+    captcha = input("输入验证码>\n")
+    return captcha
+
 
 def zhihu_login(account, password):
-    #知乎登录
-    if re.match("^1\d{10}",account):
-        print ("手机号码登录")
+    # 知乎登录
+    if re.match("^1\d{10}", account):
+        print("手机号码登录")
         post_url = "https://www.zhihu.com/login/phone_num"
         post_data = {
             "_xsrf": get_xsrf(),
             "phone_num": account,
-            "password": password
+            "password": password,
+            "captcha": get_captcha(),
         }
     else:
         if "@" in account:
-            #判断用户名是否为邮箱
+            # 判断用户名是否为邮箱
             print("邮箱方式登录")
             post_url = "https://www.zhihu.com/login/email"
             post_data = {
@@ -75,6 +103,10 @@ def zhihu_login(account, password):
     response_text = session.post(post_url, data=post_data, headers=header)
     session.cookies.save()
 
-zhihu_login("18519509255", "181171aa")
-# get_index()
+
+zhihu_login("18528509255", "181171aa")
+# # get_index()
 is_login()
+
+
+get_captcha()
